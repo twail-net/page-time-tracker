d411bc9395baa19ad62d2cfb9204d1da90361803
David Poetzsch-Heffter <davidpoetzsch@web.de>
2018-02-27 22:03:10 +0100

    removed hack

--- index.ts
+++ index.ts
@@ -1,18 +1,3 @@
-// force-fix typings
-declare module 'visibilityjs' {
-  export function every(interval: number, callback: Function);
-  export function onVisible(callback: Function);
-  export function afterPrerendering(callback: Function);
-  export function isSupported(): boolean;
-  export function state(): string;
-  export function hidden(): boolean;
-  export function unbind(callback: Function);
-  export function change(listener: VisiblityChangeListener);
-  export function stop(listener: VisiblityChangeListener);
-
-  type VisiblityChangeListener = (event, state: string) => void;
-}
-
 import * as Visibility from 'visibilityjs';
 
 export type ActionFn = (visibilityTime: number) => any;
